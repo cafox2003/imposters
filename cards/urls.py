a3d2b1af9ccf6079c767dd51e47abe0f41e6e6fe
Caleb Fox <cafox2003@gmail.com>
2025-11-22 00:55:59 -0500

    Updates ui and url handling

--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
 from . import views
+from .models import QuestionPair
+from django.shortcuts import redirect
+
+def redirect_to_first(request):
+    first =  QuestionPair.objects.first()
+    return redirect('card-detail',id=first.id)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    path("", redirect_to_first),
     path("<int:id>", views.card_detail, name="card-detail")
 ]
--- imposters/urls.py
+++ imposters/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cards/', include("cards.urls"))
+    path('cards/', include("cards.urls")),
+    path('', lambda request: redirect('/cards/'))
 ]
